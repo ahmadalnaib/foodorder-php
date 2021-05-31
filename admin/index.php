7ae8c3dc991f719de4a36b897c59646e2e67506e
Ahmed Habeeb <ahmedhabeeb@Ahmeds-Mac-mini.fritz.box>
2021-05-31 22:02:14 +0200

    add admin pages

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+
+<?php require_once('template/header.php') ?>
+  <div class="main_content">
+  <div class="col-4">Category</div>
+  <div class="col-4">Food</div>
+  <div class="col-4">Orders</div>
+  <div class="col-4">Admin </div>
+  </div>
+  <?php require_once('template/footer.php') ?>
---
+++ admin/mangeadmin.php
@@ -0,0 +1,4 @@
+
+<?php require_once('template/header.php') ?>
+<h1>mange admin</h1>
+  <?php require_once('template/footer.php') ?>
